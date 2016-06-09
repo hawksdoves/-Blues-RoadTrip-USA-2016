3272ccf8d479a577486d40e4b3fa3669fa9ffaf8
phoebe <phoebe.greig@gmail.com>
2016-06-09 19:20:07 -0400

    created function to find most current location

--- public/controllers/roadtripController.js
+++ public/controllers/roadtripController.js
@@ -5,12 +5,16 @@ roadtripApp.controller('RoadTripController', ['MapService', function(MapService)
   MapService.maps(MapService.generateMap);
 
   self.currentCity = function(){
-    return "hello";
-    //return MapService.cities.map(mostRecentCity).sort()[0]
+    return MapService.cities.filter(mostRecentCity)[0];
   }
 
   function mostRecentCity(city){
-    return city.arrival;
+    var recentArrivalDate = MapService.cities.map(arrayOfArrivalDates).sort()[0];
+    return city.arrival == recentArrivalDate;
   };
+
+  function arrayOfArrivalDates(city){
+    return city.arrival
+  }
 
 }])
